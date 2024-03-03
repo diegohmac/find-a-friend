11ded545fa37359a4a7d3c0210497ee0bfbdde2c
Diego Machado <machadodiegoh@gmail.com>
2024-03-03 19:36:56 +0000

    feat: add pets repository and register pet use case

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,35 @@
+import { randomUUID } from 'node:crypto';
+import { Pet, Prisma } from '@prisma/client';
+import { PetsRepository } from '../pets-repository';
+
+export class InMemoryPetsRepository implements PetsRepository {
+  public pets: Pet[] = [];
+
+  async findById(id: string) {
+    const pet = this.pets.find((pet) => pet.id === id);
+
+    if (!pet) {
+      return null;
+    }
+    return pet;
+  }
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const images = Array.isArray(data?.images) ? data.images : [];
+    const adoptionRequirements = Array.isArray(data?.adoptionRequirements)
+      ? data.adoptionRequirements
+      : [];
+
+    const pet = {
+      id: randomUUID(),
+      ...data,
+      about: data.about || null,
+      images,
+      adoptionRequirements,
+    };
+
+    this.pets.push(pet);
+
+    return pet;
+  }
+}
