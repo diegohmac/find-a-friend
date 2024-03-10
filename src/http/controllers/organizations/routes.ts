03a560850212a4fc012162d7d612c8abc50daadc
Diego Machado <49965743+diegohmac@users.noreply.github.com>
2024-03-10 09:25:41 +0000

    fix: change organizations routes to be public

--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -1,10 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
-import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { register } from './register';
 import { authenticate } from './authenticate';
 
 export async function organizationsRoutes(app: FastifyInstance) {
   app.post('/organizations', register);
-  app.post('/organizations/login', { preHandler: verifyJWT }, authenticate);
+  app.post('/organizations/login', authenticate);
 }
