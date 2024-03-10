7837d7c215f8c81b0be1f3c4c3a0706a7e7aa49a
Diego Machado <49965743+diegohmac@users.noreply.github.com>
2024-03-10 09:10:43 +0000

    feat: add authenticate controller

--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from 'fastify';
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { register } from './register';
+import { authenticate } from './authenticate';
 
 export async function organizationsRoutes(app: FastifyInstance) {
-  app.addHook('onRequest', verifyJWT);
-
   app.post('/organizations', register);
+  app.post('/organizations/login', { preHandler: verifyJWT }, authenticate);
 }
