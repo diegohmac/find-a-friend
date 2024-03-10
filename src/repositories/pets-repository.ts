67720cd6d2ac184675cfc8567fb48e95dc2d67da
Diego Machado <machadodiegoh@gmail.com>
2024-03-10 06:50:46 +0000

    feat: add filter options to fetch-pets use case

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,7 +1,23 @@
-import { Prisma, Pet } from '@prisma/client';
+import {
+  Prisma,
+  Pet,
+  PetAge,
+  PetSize,
+  PetEnergy,
+  PetIndependency,
+  PetEnvironment,
+} from '@prisma/client';
 
+export type PetsFilterOptions = {
+  city: string;
+  age?: PetAge;
+  size?: PetSize;
+  energy?: PetEnergy;
+  independency?: PetIndependency;
+  environment?: PetEnvironment;
+};
 export interface PetsRepository {
   findById(id: string): Promise<Pet | null>;
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
-  list(city: string): Promise<Pet[]>;
+  list(filter: PetsFilterOptions): Promise<Pet[]>;
 }
